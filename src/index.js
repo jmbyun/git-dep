b0b686284742988e17fe22fbaa0effda2cb3439d
Jeongmin Byun <jmbyun91@gmail.com>
2018-12-06 09:59:28 +0900

    fix config loading issue

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
+const path = require('path');
 const git = require('./git');
-const log = require('fancy-log')
 const ssh = require('./git-ssh');
 
 const DEFAULT_CONFIG_PATH = './git-dep.config.js';
@@ -8,26 +8,30 @@ const DEFAULT_BASE_PATH = 'git_repositories';
 
 function getConfig() {
   if (fs.existsSync(DEFAULT_CONFIG_PATH)) {
-    return require(DEFAULT_CONFIG_PATH);
+    return require(path.join(process.cwd(), DEFAULT_CONFIG_PATH));
   } else {
-    return require('./package.json').gitDep;
+    const package = require(path.join(process.cwd(), 'package.json'));
+    if (package.gitDep) {
+      return package.gitDep;
+    } else {
+      throw Error(`Failed to find the config for git-dep. Create "${DEFAULT_CONFIG_PATH}" maybe?`);
+    }
   }
 }
 
 module.exports = function installGitRepos() {
   return new Promise((resolve, reject) => {
-    getConfig().then(config => {
-      const basePath = `${process.cwd()}/${config.basePath || DEFAULT_BASE_PATH}`;
-      const dependencies = config.dependencies || {};
-      if (config.sshKeyPath) {
-        // When custom RSA private key is provided.
-        const sshKeyPath = `${process.cwd()}/${config.sshKeyPath}`;
-        ssh(sshKeyPath).then(() => {
-          git(dependencies, basePath).then(resolve, reject);
-        }, reject);
-      } else {
+    const config = getConfig();
+    const basePath = `${process.cwd()}/${config.basePath || DEFAULT_BASE_PATH}`;
+    const dependencies = config.dependencies || {};
+    if (config.sshKeyPath) {
+      // When custom RSA private key is provided.
+      const sshKeyPath = `${process.cwd()}/${config.sshKeyPath}`;
+      ssh(sshKeyPath).then(() => {
         git(dependencies, basePath).then(resolve, reject);
-      }
-    }, reject);
+      }, reject);
+    } else {
+      git(dependencies, basePath).then(resolve, reject);
+    }
   });
 };
